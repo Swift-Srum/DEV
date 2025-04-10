b9323507ec65f0a36401fc2c9b01e330aa8c00af
AdanBG <120198724+AdanBG@users.noreply.github.com>
2025-04-10 16:28:55 +0100

    To make it work with plesk

--- essential/config.php
+++ essential/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
-	define('DB_NAME', 'bowsers');
+	define('DB_USER', 'tester2');
+	define('DB_PASS', '6eKF9zQRYmqo%-*9');
+	define('DB_NAME', 'bowsers_assignment1');
 ?>
